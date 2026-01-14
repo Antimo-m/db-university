8e716f470d354e2b9ce5c72e0822d28cb5adbd8a
Antimo-M <montellaantimo@live.it>
2026-01-14 17:12:58 +0100

    Bonus

--- text.sql
+++ text.sql
@@ -40,3 +40,25 @@ FROM `departments`
 SELECT COUNT(*) AS "RISULTATO"
 FROM `teachers`
 WHERE `phone` IS NULL
+
+
+
+--BONUS
+--1.Contare quanti iscritti ci sono stati ogni anno
+SELECT YEAR(enrolment_date) As "anno", COUNT(*) AS "iscritti"
+FROM `students`
+GROUP BY YEAR(enrolment_date)
+ORDER BY anno
+--2.Contare gli insegnanti che hanno l'ufficio nello stesso edificio
+SELECT `office_address`, COUNT(*) AS "numero_insegnanti"
+FROM `teachers`
+GROUP BY `office_address`
+ORDER BY numero_insegnanti DESC
+--3.Calcolare la media dei voti di ogni appello d'esame
+SELECT `exam_id`, AVG(`vote`) 
+FROM `exam_student`
+GROUP BY `exam_id`
+--4.Contare quanti corsi di laurea ci sono per ogni dipartimento
+SELECT `department_id`, COUNT(*) AS "NUMERO DI CORSI"
+FROM `degrees`
+GROUP BY `department_id`
