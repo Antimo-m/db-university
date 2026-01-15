1bcf2c28ac9ce6aa62391cefda30743b90770b2c
Antimo-M <montellaantimo@live.it>
2026-01-16 00:13:44 +0100

    aggiunto nell'es 7 HAVING MAX cosi da prendere quei studenti che hanno superato l'esame alemeno una volta.

--- text2.sql
+++ text2.sql
@@ -72,6 +72,9 @@ ON exams.id = exam_student.exam_id
 INNER JOIN courses
 ON courses.id = exams.course_id
 GROUP BY students.id, students.name, students.surname, courses.name
+HAVING MAX(exam_student.vote) >= 18 --andiamo a prendere quelli che sono stati superati almeno una volta
 ORDER BY students.id
+
+
 
 
